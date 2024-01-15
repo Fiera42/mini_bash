f9f387211588ea910e214a1f72e93aea66965786
Fiera42 <anaigeon@gmx.fr>
2024-01-15 11:38:46 +0100

    Squelette lecture de commande

--- src/mbash.c
+++ src/mbash.c
@@ -3,21 +3,75 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+
+
 #define MAXLI 2048
-char cmd[MAXLI];
 char path[MAXLI];
 int pathidx;
+
 void mbash();
+char** parseCmd();
+
 int main(int argc, char** argv) {
+  char cmd[MAXLI];
+
   while (1) {
     printf("Commande: ");
     fgets(cmd, MAXLI, stdin);
-    mbash(cmd);
+
+    char** p_cmd = parseCmd(cmd);
+
+    mbash(p_cmd);
+
+    for (int i = 0; p_cmd[i] != NULL; i++) {
+      free(p_cmd[i]);
+    }
+    free(p_cmd);
   }
   return 0;
 }
 
-void mbash() {
-  printf("Execute: %s", cmd);
-  system(cmd);
+void mbash(char** p_cmd) {
+  printf("Execute: %s ", p_cmd[0]);
+
+  int p_cmd_size = 0;
+  while (p_cmd[p_cmd_size] != NULL) {
+    p_cmd_size++;
+  }
+
+  printf(" size : %i", p_cmd_size);
+  printf("\n");
+
+  /*
+  
+  if(p_cmd_size > 1) {
+    for(int i = 1; i < p_cmd_size; i++) {
+      printf("arg %i : %s", i, p_cmd[i]);
+    }
+  }
+  */
+
+  //system(p_cmd[0]);
+}
+
+char** parseCmd(char* cmd) {
+  //Allocation pour le nouveau tableau
+  char** p_cmd = (char**)malloc(MAXLI * sizeof(char*));
+  for (int i = 0; i < MAXLI; i++) {
+    p_cmd[i] = (char*)malloc(MAXLI * sizeof(char));
+  }
+
+  //Parse
+  char* token = strtok(cmd, " \t\n");
+
+  //Ecriture
+  int i = 0;
+  while (token != NULL) {
+    strcpy(p_cmd[i], token);
+    i++;
+    token = strtok(NULL, " \t\n");
+  }
+  p_cmd[i] = NULL;
+
+  return p_cmd;
 }
